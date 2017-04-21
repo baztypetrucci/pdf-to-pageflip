226366bc4f6a179eaeba255f73b0181b480a73dd
bastian <bastian@mitocondria.cl>
2017-04-21 11:52:49 -0300

    scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
 		footer:'.bs-docs-footer',
 		delta:200
 	});
+	$('.background-loader, .loaderGif').hide();
+	$('#formUploader').submit(function(e){
+		$('.background-loader, .loaderGif').fadeIn(500);
+	});
 	$('form').validator().on('submit', function (e) {
 		if (e.isDefaultPrevented()) {
 			// handle the invalid form...
 		} else {
 			$(this).submit();
 		}
-	});
-	$('.background-loader, .loaderGif').hide();
-	$('#formUploader').submit(function(e){
-		$('.background-loader, .loaderGif').fadeIn(500);
 	});
 });
